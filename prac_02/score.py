f6888fa784ee409796052661584d10f78f644566
Candice Christabel <candice.christabel@my.jcu.edu.au>
2025-02-09 20:32:59 +0800

    Add result function and generate random score

---
+++ prac_02/score.py
@@ -0,0 +1,24 @@
+import random
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+def main():
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
+    else:
+        print(determine_result(score))
+    score = random.randint(0, 100)
+    print(f"Random score: {score}\n{determine_result(score)}")
+
+
+def determine_result(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
