efdb6a5c53662979b7f38182d08d2a5cf8376575
Candice Christabel <candice.christabel@my.jcu.edu.au>
2025-02-14 18:19:03 +0800

    Answered 3 questions and add program for random numbers

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 - Practical
+Answers for random functions in console and
+code to print a random number from 1 to 100
+"""
+"""
+1. I saw the number 19 for line 1, the smallest number I could have seen is 5 and the largest number would be 20.
+2. I saw "3" on line 3, the smallest number I could have seen is 3, and the largest number would be 9. Line 2 couldn't produce 4 because the line 2 can only produce number from 3 to 9 in steps of 2, so the only numbers that can appear are 3, 5, 7, and 9.
+3. I saw the number 4.152533511615266 on line 3. The smallest number I could have seen is 2.5 and the largest number would be 5.5.
+"""
+import random
+print(random.randint(1, 100))
