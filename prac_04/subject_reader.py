183bd4bb5c6eac3df11d907946c4ee9cc89452af
Candice Christabel <candice.christabel@my.jcu.edu.au>
2025-02-21 18:24:12 +0800

    Add return function and new function to display details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,21 +9,25 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     print(data)
+    print_subject_details(data)
 
+def print_subject_details(data):
+    """display subject details from data"""
+    for parts in data:
+        print(f"{parts[0]} is taught by {parts[1]:<12} and has {parts[2]:>3} students")
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    merged_parts = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        merged_parts.append(parts)
     input_file.close()
+    return merged_parts  # See if that worked
+
 
 
 main()
