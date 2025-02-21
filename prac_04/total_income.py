59036cad2fed66c0fd79a6b943ead3a8980f9b2f
Candice Christabel <candice.christabel@my.jcu.edu.au>
2025-02-21 18:25:37 +0800

    Used f-string, changed variable name, refactored

---
+++ prac_04/total_income.py
@@ -0,0 +1,30 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+
+    print_report(incomes)
+
+
+def print_report(incomes):
+    """Display income report"""
+    print("\nIncome Report\n-------------")
+    total = 0
+    month = 0
+    for income in incomes:
+        total += income
+        month += 1
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
